2ea19a4d4ab6f9cfbaf06493de4585cf8336243d
jdgwartney <davidg@boundary.com>
2015-09-05 19:17:11 -0700

    Add templates

--- createfile/recipes/default.rb
+++ createfile/recipes/default.rb
@@ -11,3 +11,13 @@ cookbook_file "/srv/www/shared/example_data.json" do
   mode 0644
   action :create_if_missing
 end
+
+template "/srv/www/shared/example_data_template.json" do
+  source "example_data.json.erb"
+  mode 0644
+  variables(
+    :a_boolean_var => true,
+    :a_string_var => "some string"
+  )
+  only_if {node['createfile']['install_file']}
+end
